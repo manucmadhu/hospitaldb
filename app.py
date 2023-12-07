ef881a7552f160e4e19ac322eb6b3da42029415d
manucmadhu <125571706+manucmadhu@users.noreply.github.com>
2023-12-07 20:15:10 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask import Flask, render_template, request, jsonify
 from back import create, addD, delD, addP, delP, addA, delA, addM, delM, addPh, delPh, selD, selA, selP, selPh, selM
-# import Flask
-# import back
+
 app = Flask(__name__)
 
 create()
@@ -10,171 +9,162 @@ create()
 def index():
     return render_template('index.html')
 
+# Routes for Doctors
 @app.route('/doctors')
 def doctors():
     doctors_data = selD(-1)
     return render_template('entity.html', entity_type='Doctor', data=doctors_data)
 
-@app.route('/patients')
-def patients():
-    patients_data = selP(-1)
-    return render_template('entity.html', entity_type='Patient', data=patients_data)
-
-@app.route('/pharmacies')
-def pharmacies():
-    pharmacies_data = selPh(-1)
-    return render_template('entity.html', entity_type='Pharmacy', data=pharmacies_data)
-
 @app.route('/add_doctor', methods=['POST'])
 def add_doctor():
     if request.method == 'POST':
-        doc_id = int(request.form['entityId'])
-        doc_name = request.form['entityName']
-        # Get other form inputs and call the corresponding database function
-        addD(doc_id, doc_name, ...)
-        return jsonify({'success': True})
+        try:
+            doc_id = int(request.form['entityId'])
+            doc_name = request.form['entityName']
+            department = request.form['department']
+            opdf = request.form['opdf']
+            opdt = request.form['opdt']
+            room_no = int(request.form['roomNo'])
+            phone_no = request.form['phoneNo']
 
-# Similar routes for other entities (add_patient, add_appointment, add_medicine, add_pharmacy)
+            addD(doc_id, doc_name, department, opdf, opdt, room_no, phone_no)
+
+            return jsonify({'success': True})
+        except Exception as e:
+            return jsonify({'success': False, 'error': str(e)})
 
 @app.route('/delete_doctor/<int:doc_id>')
 def delete_doctor(doc_id):
     delD(doc_id)
     return jsonify({'success': True})
-# Similar functions for other tables (addP, delP, addA, delA, addM, delM, addPh, delPh, selD, selA, selP, selPh, selM)
 
-# Add and delete functions for PATIENT table
-def addP(a, b, c, d):
-    create()
-    conn = sqlite3.connect(database="Hospital.db")
-    cur = conn.cursor()
-    cur.execute("INSERT INTO PATIENT(ID, NAME, PH_NO, IO) VALUES(?,?,?,?)", (a, b, c, d))
-    cur.close()
-    conn.commit()
-    conn.close()
+# Routes for Patients
+@app.route('/patients')
+def patients():
+    patients_data = selP(-1)
+    return render_template('entity.html', entity_type='Patient', data=patients_data)
 
-def delP(a):
-    create()
-    conn = sqlite3.connect(database="Hospital.db")
-    cur = conn.cursor()
-    cur.execute("DELETE FROM PATIENT WHERE ID=?", (a,))
-    cur.close()
-    conn.commit()
-    conn.close()
+@app.route('/add_patient', methods=['POST'])
+def add_patient():
+    if request.method == 'POST':
+        try:
+            patient_id = int(request.form['entityId'])
+            patient_name = request.form['entityName']
+            phone_no = request.form['phoneNo']
+            io = request.form['io']
 
-# Add and delete functions for APPOINTMENT table
-def addA(a, b, c, d):
-    create()
-    conn = sqlite3.connect(database="Hospital.db")
-    cur = conn.cursor()
-    cur.execute("INSERT INTO APPOINTMENT(ID, P_ID, D_ID, TOKEN) VALUES(?,?,?,?)", (a, b, c, d))
-    cur.close()
-    conn.commit()
-    conn.close()
+            addP(patient_id, patient_name, phone_no, io)
 
-def delA(a):
-    create()
-    conn = sqlite3.connect(database="Hospital.db")
-    cur = conn.cursor()
-    cur.execute("DELETE FROM APPOINTMENT WHERE ID=?", (a,))
-    cur.close()
-    conn.commit()
-    conn.close()
+            return jsonify({'success': True})
+        except Exception as e:
+            return jsonify({'success': False, 'error': str(e)})
 
-# Add and delete functions for MEDICINE table
-def addM(a, b):
-    create()
-    conn = sqlite3.connect(database="Hospital.db")
-    cur = conn.cursor()
-    cur.execute("INSERT INTO MEDICINE(M_ID, NAME) VALUES(?,?)", (a, b))
-    cur.close()
-    conn.commit()
-    conn.close()
+@app.route('/delete_patient/<int:patient_id>')
+def delete_patient(patient_id):
+    delP(patient_id)
+    return jsonify({'success': True})
 
-def delM(a):
-    create()
-    conn = sqlite3.connect(database="Hospital.db")
-    cur = conn.cursor()
-    cur.execute("DELETE FROM MEDICINE WHERE M_ID=?", (a,))
-    cur.close()
-    conn.commit()
-    conn.close()
+# Routes for Appointments
+@app.route('/appointments')
+def appointments():
+    appointments_data = selA(-1)
+    return render_template('entity.html', entity_type='Appointment', data=appointments_data)
 
-# Add and delete functions for PHARMACY table
-def addPh(a, b, c, d, e):
-    create()
-    conn = sqlite3.connect(database="Hospital.db")
-    cur = conn.cursor()
-    cur.execute("INSERT INTO PHARMACY(P_ID, MED1, MED2, MED3, MED4) VALUES(?,?,?,?,?)", (a, b, c, d, e))
-    cur.close()
-    conn.commit()
-    conn.close()
+@app.route('/add_appointment', methods=['POST'])
+def add_appointment():
+    if request.method == 'POST':
+        try:
+            appointment_id = int(request.form['entityId'])
+            patient_id = int(request.form['patientId'])
+            doctor_id = int(request.form['doctorId'])
+            token = int(request.form['token'])
 
-def delPh(a):
-    create()
-    conn = sqlite3.connect(database="Hospital.db")
-    cur = conn.cursor()
-    cur.execute("DELETE FROM PHARMACY WHERE P_ID=?", (a,))
-    cur.close()
-    conn.commit()
-    conn.close()
+            addA(appointment_id, patient_id, doctor_id, token)
 
-# Select functions for each table
-def selD(a):
-    create()
-    conn = sqlite3.connect(database="Hospital.db")
-    cur = conn.cursor()
-    if a == -1:
-        cur.execute("SELECT * FROM DOCTOR")
-    else:
-        cur.execute("SELECT * FROM DOCTOR WHERE ID=?", (a,))
-    rows = cur.fetchall()
-    return rows
+            return jsonify({'success': True})
+        except Exception as e:
+            return jsonify({'success': False, 'error': str(e)})
 
-def selP(a):
-    create()
-    conn = sqlite3.connect(database="Hospital.db")
-    cur = conn.cursor()
-    if a == -1:
-        cur.execute("SELECT * FROM PATIENT")
-    else:
-        cur.execute("SELECT * FROM PATIENT WHERE ID=?", (a,))
-    rows = cur.fetchall()
-    return rows
+@app.route('/delete_appointment/<int:appointment_id>')
+def delete_appointment(appointment_id):
+    delA(appointment_id)
+    return jsonify({'success': True})
 
-def selA(a):
-    create()
-    conn = sqlite3.connect(database="Hospital.db")
-    cur = conn.cursor()
-    if a == -1:
-        cur.execute("SELECT * FROM APPOINTMENT")
-    else:
-        cur.execute("SELECT * FROM APPOINTMENT WHERE ID=?", (a,))
-    rows = cur.fetchall()
-    return rows
+# Routes for Medicines
+@app.route('/medicines')
+def medicines():
+    medicines_data = selM(-1)
+    return render_template('entity.html', entity_type='Medicine', data=medicines_data)
 
-def selM(a):
-    create()
-    conn = sqlite3.connect(database="Hospital.db")
-    cur = conn.cursor()
-    if a == -1:
-        cur.execute("SELECT * FROM MEDICINE")
-    else:
-        cur.execute("SELECT * FROM MEDICINE WHERE M_ID=?", (a,))
-    rows = cur.fetchall()
-    return rows
+@app.route('/add_medicine', methods=['POST'])
+def add_medicine():
+    if request.method == 'POST':
+        try:
+            medicine_id = int(request.form['entityId'])
+            medicine_name = request.form['entityName']
 
-def selPh(a):
-    create()
-    conn = sqlite3.connect(database="Hospital.db")
-    cur = conn.cursor()
-    if a == -1:
-        cur.execute("SELECT * FROM PHARMACY")
-    else:
-        cur.execute("SELECT * FROM PHARMACY WHERE P_ID=?", (a,))
-    rows = cur.fetchall()
-    return rows
+            addM(medicine_id, medicine_name)
 
-# Similar routes for other entities (delete_patient, delete_appointment, delete_medicine, delete_pharmacy)
+            return jsonify({'success': True})
+        except Exception as e:
+            return jsonify({'success': False, 'error': str(e)})
+
+@app.route('/delete_medicine/<int:medicine_id>')
+def delete_medicine(medicine_id):
+    delM(medicine_id)
+    return jsonify({'success': True})
+
+# Routes for Pharmacies
+@app.route('/pharmacies')
+def pharmacies():
+    pharmacies_data = selPh(-1)
+    return render_template('entity.html', entity_type='Pharmacy', data=pharmacies_data)
+
+@app.route('/add_pharmacy', methods=['POST'])
+def add_pharmacy():
+    if request.method == 'POST':
+        try:
+            pharmacy_id = int(request.form['entityId'])
+            med1_id = int(request.form['med1Id'])
+            med2_id = int(request.form['med2Id'])
+            med3_id = int(request.form['med3Id'])
+            med4_id = int(request.form['med4Id'])
+
+            addPh(pharmacy_id, med1_id, med2_id, med3_id, med4_id)
+
+            return jsonify({'success': True})
+        except Exception as e:
+            return jsonify({'success': False, 'error': str(e)})
+
+@app.route('/delete_pharmacy/<int:pharmacy_id>')
+def delete_pharmacy(pharmacy_id):
+    delPh(pharmacy_id)
+    return jsonify({'success': True})
+
+@app.route('/select_doctor/<int:doc_id>')
+def select_doctor(doc_id):
+    doctor_data = selD(doc_id)
+    return jsonify({'success': True, 'data': doctor_data})
+
+@app.route('/select_patient/<int:patient_id>')
+def select_patient(patient_id):
+    patient_data = selP(patient_id)
+    return jsonify({'success': True, 'data': patient_data})
+
+@app.route('/select_appointment/<int:appointment_id>')
+def select_appointment(appointment_id):
+    appointment_data = selA(appointment_id)
+    return jsonify({'success': True, 'data': appointment_data})
+
+@app.route('/select_medicine/<int:medicine_id>')
+def select_medicine(medicine_id):
+    medicine_data = selM(medicine_id)
+    return jsonify({'success': True, 'data': medicine_data})
+
+@app.route('/select_pharmacy/<int:pharmacy_id>')
+def select_pharmacy(pharmacy_id):
+    pharmacy_data = selPh(pharmacy_id)
+    return jsonify({'success': True, 'data': pharmacy_data})
 
 if __name__ == '__main__':
     app.run(debug=True)
